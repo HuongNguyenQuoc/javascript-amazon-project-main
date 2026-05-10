e1cc6d4216b90b53b7d7e9c86e39105fc94068b7
HuongNguyenQuoc <quochuong05zz@gmail.com>
2026-05-10 16:31:13 +0700

    Use modules and import cart from card.js

--- 13-js-amazon-project/scripts/amazon.js
+++ 13-js-amazon-project/scripts/amazon.js
@@ -1,3 +1,5 @@
+import {cart as myCart} from '../data/cart.js';
+
 let productsHTML = '';
 
 products.forEach((product) => {
@@ -39,7 +41,7 @@ products.forEach((product) => {
 
         <div class="product-spacer"></div>
 
-        <div class="added-to-cart">
+        <div class="added-to-cart js-added-to-cart-${product.id}">
           <img src="images/icons/checkmark.png">
           Added
         </div>
@@ -54,10 +56,12 @@ products.forEach((product) => {
 document.querySelector('.js-products-grid')
 .innerHTML = productsHTML;
 
+const addedMessageTimeouts = {};
+
 document.querySelectorAll('.js-add-to-cart')
 .forEach((button) => {
   button.addEventListener('click', () => {
-    const productId = button.dataset.productId;
+    const { productId } = button.dataset; //Create a name variable to the same with property in button.datasets
 
     let matchingItem;
 
@@ -75,8 +79,8 @@ document.querySelectorAll('.js-add-to-cart')
       matchingItem.quantity += quantity;
     } else {
         cart.push({
-          productId: productId,
-          quantity: quantity
+          productId,
+          quantity
       });
     }
 
@@ -87,7 +91,23 @@ document.querySelectorAll('.js-add-to-cart')
     });
 
     document.querySelector('.js-cart-quantity').innerHTML = cartQuantity;
-    console.log(cartQuantity);
-    console.log(cart);
+
+    const addedMessage = document.querySelector(
+      `.js-added-to-cart-${productId}`
+    );
+
+    addedMessage.classList.add('added-to-cart-visible');
+
+    const previousTimeoutId = addedMessageTimeouts[productId];
+
+    if (previousTimeoutId) {
+      clearTimeout(previousTimeoutId);
+    }
+
+    const timeoutId = setTimeout(() => {
+      addedMessage.classList.remove('added-to-cart-visible');
+    }, 2000); 
+
+    addedMessageTimeouts[productId] = timeoutId;
   });
 });
