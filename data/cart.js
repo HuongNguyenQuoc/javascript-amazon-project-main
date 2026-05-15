edc85b580e9b26a5a2af66dc4c331c36ef7f3ea1
HuongNguyenQuoc <quochuong05zz@gmail.com>
2026-05-15 10:19:45 +0700

    Create automated tests for addToCart

--- data/cart.js
+++ data/cart.js
@@ -1,5 +1,10 @@
-export let cart = JSON.parse(localStorage.getItem('cart')) || [{
-    productId : 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
+export let cart;
+
+loadFromStorage();
+
+export function loadFromStorage() {
+  cart = JSON.parse(localStorage.getItem('cart')) || [{
+    productId: 'e43638ce-6aa0-4b85-b27f-e1d07eb678c6',
     quantity: 2,
     deliveryOptionId: '1'
   }, {
@@ -7,6 +12,7 @@ export let cart = JSON.parse(localStorage.getItem('cart')) || [{
     quantity: 1,
     deliveryOptionId: '2'
   }];
+}
 
 export function saveToStorage() {
   localStorage.setItem('cart', JSON.stringify(cart));
@@ -22,10 +28,10 @@ export function addToCart(productId, quantity) {
   if (matchingItem) {
     matchingItem.quantity += quantity;
   } else {
-      cart.push({
-        productId,
-        quantity,
-        deliveryOptionId: '1'
+    cart.push({
+      productId,
+      quantity,
+      deliveryOptionId: '1'
     });
   }
 
@@ -53,7 +59,7 @@ export function updateQuantity(productId, newQuantity) {
   cart.forEach((cartItem) => {
     if (cartItem.productId === productId) matchingItem = cartItem;
   });
-  
+
   matchingItem.quantity = newQuantity;
 
   saveToStorage();
