dadf82751f4aa6f77df43aac6297753b09b01559
HuongNguyenQuoc <quochuong05zz@gmail.com>
2026-05-18 19:08:35 +0700

    Learn error handling

--- data/products.js
+++ data/products.js
@@ -771,6 +771,8 @@ export function loadProductsFetch() {
     });
     
     console.log('load products');
+  }).catch((error) => {
+    console.log('Unexpected error. Please try again later.');
   });
 
   return promise;
@@ -787,6 +789,10 @@ export function loadProducts(fun) {
     });
     fun();
   });
+
+  xhr.addEventListener('error', (error) => {
+    console.log('Unexpected error. Please try again later.');
+  })
 
   xhr.open('GET', 'https://supersimplebackend.dev/products');
   xhr.send();
